c1ae93de5f149fbeca70dace7c0481c9a90d4178
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-12 11:41:02 -0700

    HTTPServer#determine_endpoint method added

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -16,12 +16,28 @@ class HTTPServer
       Thread.new(@server.accept) do |client|
         body = response_body("Hello, World! (#{@times += 1})", client)
         headers = response_headers(body)
-
+        determine_endpoint client
         client.puts headers
         client.puts body
         client.close
       end
     end
+  end
+
+  def determine_endpoint(endpoint)
+    result = { method: nil, endpoint: nil }
+    if endpoint.slice(0, 3) == 'GET'
+      result[:method] = 'GET'
+      endpoint.slice!(0, 4)
+      endpoint.slice!(endpoint.length - 9, endpoint.length - 1)
+      result[:endpoint] = endpoint
+    elsif endpoint.slice(0, 4) == 'POST'
+      result[:method] = 'POST'
+      endpoint.slice!(0, 5)
+      endpoint.slice!(endpoint.length - 9, endpoint.length - 1)
+      result[:endpoint] = endpoint
+    end
+    result
   end
 
   def response_body(output, client)
--- test/http_server_test.rb
+++ test/http_server_test.rb
@@ -10,4 +10,13 @@ class HTTPServerTest < Minitest::Test
     assert_instance_of TCPServer, SERVER.server
     SERVER.close
   end
+
+  def test_does_determine_endpoint
+    expected_get = { method: 'GET', endpoint: '/test' }
+    expected_post = { method: 'POST', endpoint: '/test' }
+    expected_nil = { method: nil, endpoint: nil }
+    assert_equal expected_get, SERVER.determine_endpoint('GET /test HTTP/1.1')
+    assert_equal expected_post, SERVER.determine_endpoint('POST /test HTTP/1.1')
+    assert_equal expected_nil, SERVER.determine_endpoint('DELETE /test HTTP/1.1')
+  end
 end
