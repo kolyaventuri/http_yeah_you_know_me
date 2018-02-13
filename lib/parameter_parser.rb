16c8239ba5b7e68d3e4e316035650b786d14d669
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-13 10:18:58 -0700

    Parsing code for singular parameter

--- lib/parameter_parser.rb
+++ lib/parameter_parser.rb
@@ -2,11 +2,22 @@
 class ParameterParser
   def parse(path)
     parts = split_parts path
-    { path: parts[:path], parameters: parts[:param_string] }
+    parameters = split_parameters parts[:param_string]
+    { path: parts[:path], parameters: parameters }
   end
 
   def split_parts(path)
     parts = path.split '?'
     { path: parts[0], param_string: parts[1] }
+  end
+
+  def split_parameters(parameters)
+    return nil if parameters.nil?
+    parts = parameters.split '='
+    params = {}
+
+    params[parts[0]] = parts[1]
+
+    params
   end
 end
