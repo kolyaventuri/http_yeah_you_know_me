918600ade8aa13eacae92028bc9512c52d78ffab
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-13 11:34:29 -0700

    More router tests

--- test/router_class_test.rb
+++ test/router_class_test.rb
@@ -3,12 +3,29 @@ require_relative 'test_helper.rb'
 require './lib/routers/_router.rb'
 
 class RouterClassTest < Minitest::Test
-  def test_does_create_router
-    router = Router.new(:GET)
-
-    assert_instance_of Router, router
-    assert_equal :GET, router.method
+  def setup
+    @router = Router.new(:GET)
   end
 
-  
+  def test_does_create_router
+    assert_instance_of Router, @router
+    assert_equal :GET, @router.method
+  end
+
+  def test_does_accept_different_methods
+    router = Router.new(:POST)
+
+    assert_instance_of Router, router
+    assert_equal :POST, router.method
+  end
+
+  def test_does_take_endpoints
+    route = @router.set '/example', (proc do |req, _res|
+      req
+    end)
+
+    assert_instance_of Proc, route
+
+    assert_equal true, @router.set?('/example')
+  end
 end
