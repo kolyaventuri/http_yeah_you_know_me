e98c3533bb510e71ffa92cf0b1ae59ce578f9577
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-13 10:14:31 -0700

    Parses path with nil parameters

--- lib/parameter_parser.rb
+++ lib/parameter_parser.rb
@@ -1,6 +1,12 @@
 # Parses endpoint parameters
 class ParameterParser
   def parse(path)
+    parts = split_parts path
+    { path: parts[:path], parameters: parts[:param_string] }
+  end
 
+  def split_parts(path)
+    parts = path.split '?'
+    { path: parts[0], param_string: parts[1] }
   end
 end
