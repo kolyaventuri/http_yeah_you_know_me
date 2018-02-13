4466a07a2905a38d4f47e2fd83a9d00ff5e9e98d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-13 11:36:55 -0700

    Router accepts endpoints

--- lib/routers/_router.rb
+++ lib/routers/_router.rb
@@ -1,8 +1,17 @@
 class Router
   attr_reader :method
-  
+
   def initialize(method)
     @method = method
     @endpoints = {}
+  end
+
+  def set(endpoint, handler)
+    @endpoints[endpoint] = handler
+  end
+
+  def set?(endpoint)
+    return true if @endpoints[endpoint]
+    false
   end
 end
