9459d90ef819a154cc68769d33ba23d8a9ff358c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-13 13:07:50 -0700

    Removed server code from runner test

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -8,40 +8,4 @@ class RunnerTest < Minitest::Test
     assert_instance_of Router, runner.router
     runner.stop
   end
-
-  def test_does_run_server
-    runner = Runner.new
-    router = runner.router
-    hello_times = 0
-    request_count = 0
-
-    router.get '*', (proc do
-      request_count += 1
-    end)
-
-    router.get '/', (proc do |_req, res|
-      res.send ''
-    end)
-
-    router.get '/hello', (proc do |_req, res|
-      res.send "Hello, world! (#{hello_times += 1})"
-    end)
-
-    router.get '/datetime', (proc do |_req, res|
-      res.send Time.now.strftime('%l:%M%p on %A, %B %e, %Y')
-    end)
-
-    router.get '/shutdown', (proc do |_req, res|
-      res.send "Total Requests: #{request_count}"
-      runner.server.close
-    end)
-
-    router.get '/word_search', (proc do |req, res|
-      word = req.params['word']
-      return res.send 'No word supplied' if word.nil?
-      res.send word
-    end)
-
-    runner.start
-  end
 end
