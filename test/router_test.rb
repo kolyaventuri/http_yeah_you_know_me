c3c86a3339c0eb9ced9bb05621b38129bb7fa24a
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-13 11:44:12 -0700

    Prepped Router for refactor. Reduced router tests down to one test for simplicity

--- test/router_test.rb
+++ test/router_test.rb
@@ -14,31 +14,11 @@ class RouterTest < Minitest::Test
     end)
     assert_instance_of Proc, get_route
 
-    assert_equal true, router.get_set?('/example')
-    assert_equal false, router.set?('POST', '/example')
     assert_equal true, router.set?('GET', '/example')
+    assert_equal false, router.set?('POST', '/example')
 
     client = MockClient.new
 
-    resulting_request = router.execute(client)
-
-    assert_instance_of Request, resulting_request
-    assert_equal '/example', resulting_request.path
-  end
-
-  def test_router_can_take_post_endpoints
-    router = Router.new
-    post_route = router.post '/example', (proc do |arg|
-      arg
-    end)
-    assert_instance_of Proc, post_route
-
-    assert_equal true, router.post_set?('/example')
-    assert_equal false, router.set?('GET', '/example')
-    assert_equal true, router.set?('POST', '/example')
-
-    client = MockClient.new :POST
-    
     resulting_request = router.execute(client)
 
     assert_instance_of Request, resulting_request
