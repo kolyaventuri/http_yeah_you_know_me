420d699811e563aacb11abf76e32ca6fdced7cfe
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-13 09:33:18 -0700

    Router test fixes to work with MockClient

--- lib/mock_client.rb
+++ lib/mock_client.rb
@@ -1,10 +1,12 @@
 # Defines headers for client unit testing
 class MockClient
   attr_reader :read_out, :output
+  attr_accessor :method
 
-  def initialize
+  def initialize(method=:GET)
+    @method = method
     @read_out = headers
-    @output = ""
+    @output = ''
   end
 
   def gets
@@ -20,8 +22,29 @@ class MockClient
   end
 
   def headers
+    return get_headers if @method == :GET
+    return post_headers if @method == :POST
+    nil
+  end
+
+  def get_headers
     [
       'GET /example HTTP/1.1',
+      'Host: localhost:9292',
+      'Connection: keep-alive',
+      'Cache-Control: no-cache',
+      'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+      'Postman-Token: 74b2f6f9-d389-f2b4-83c4-c2c9e3569713',
+      'Accept: */*',
+      'DNT: 1',
+      'Accept-Encoding: gzip, deflate, br',
+      'Accept-Language: en-US,en;q=0.9'
+    ]
+  end
+
+  def post_headers
+    [
+      'POST /example HTTP/1.1',
       'Host: localhost:9292',
       'Connection: keep-alive',
       'Cache-Control: no-cache',
--- test/router_test.rb
+++ test/router_test.rb
@@ -1,36 +1,47 @@
 require_relative 'test_helper.rb'
 require 'pry'
 
+require './lib/mock_client'
+require './lib/client/request'
 require './lib/router.rb'
 
 class RouterTest < Minitest::Test
 
   def test_router_can_take_get_endpoints
     router = Router.new
-    get_route = router.get '/dummy', (proc do |arg|
-      arg
+    get_route = router.get '/example', (proc do |req, _res|
+      req
     end)
     assert_instance_of Proc, get_route
 
-    assert_equal true, router.get_set?('/dummy')
-    assert_equal false, router.set?('POST', '/dummy')
-    assert_equal true, router.set?('GET', '/dummy')
+    assert_equal true, router.get_set?('/example')
+    assert_equal false, router.set?('POST', '/example')
+    assert_equal true, router.set?('GET', '/example')
 
-    assert_equal ['Hello', 'world'], router.execute('GET', '/dummy', ['Hello', 'world'])
+    client = MockClient.new
+
+    resulting_request = router.execute(client)
+
+    assert_instance_of Request, resulting_request
+    assert_equal '/example', resulting_request.path
   end
 
   def test_router_can_take_post_endpoints
     router = Router.new
-    post_route = router.post '/dummy', (proc do |arg|
+    post_route = router.post '/example', (proc do |arg|
       arg
     end)
     assert_instance_of Proc, post_route
 
-    assert_equal true, router.post_set?('/dummy')
-    assert_equal false, router.set?('GET', '/dummy')
-    assert_equal true, router.set?('POST', '/dummy')
+    assert_equal true, router.post_set?('/example')
+    assert_equal false, router.set?('GET', '/example')
+    assert_equal true, router.set?('POST', '/example')
 
-    assert_equal ['Hello', 'world'], router.execute('POST', '/dummy', ['Hello', 'world'])
+    client = MockClient.new :POST
+    
+    resulting_request = router.execute(client)
+
+    assert_instance_of Request, resulting_request
+    assert_equal '/example', resulting_request.path
   end
-
 end
